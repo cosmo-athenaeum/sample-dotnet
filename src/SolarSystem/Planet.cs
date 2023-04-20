a94dc16137b93487f9c4814023fadf8f3066c8b0
Richard <20459042+SignalRichard@users.noreply.github.com>
2023-04-20 12:03:33 -0400

    feat: sample .net library

---
+++ src/SolarSystem/Planet.cs
@@ -0,0 +1,49 @@
+﻿namespace SolarSystem
+{
+    /// <summary>
+    /// Represents detailed information about a planet and it's characteristics.
+    /// </summary>
+    public class Planet
+    {
+        /// <summary>
+        /// The name of the planet.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Description of the planet.
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// The point in the elliptical orbit of a planet, comet, etc., where it is farthest from the Sun.
+        /// </summary>
+        public int Aphelion { get; set; }
+
+        /// <summary>
+        /// The point in the elliptical orbit of a comet, planet, etc., where it is nearest to the Sun.
+        /// </summary>
+        public int Perihelion { get; set; }
+
+        /// <summary>
+        /// The amount by which its orbit around another body deviates from a perfect circle.
+        /// </summary>
+        public double Eccentricity { get; set; }
+
+        /// <summary>
+        /// The amount of time the planet takes to complete one orbit around the Sun.
+        /// </summary>
+        public double OrbitalPeroid { get; set; }
+
+        /// <summary>
+        /// The speed at which the planet orbits around the Sun.
+        /// </summary>
+        public double AverageOrbitalSpeed { get; set; }
+
+        /// <summary>
+        /// The number of natural satelleties orbiting the planet.
+        /// </summary>
+        public int Satellites { get; set; }
+
+    }
+}
---
+++ src/SolarSystem/SolarSystem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolarSystem
+{
+    /// <summary>
+    /// Contains the names of the major bodies in the solar system including all former and current planets.
+    /// </summary>
+    public enum SolarSystem
+    {
+        Sun = 0,
+        Mecury,
+        Venus,
+        Earth,
+        Ceres,
+        Mars,
+        Jupiter,
+        Saturn,
+        Uranus,
+        Neptune,
+        Pluto
+    }
+}
